0892e7c73782f933e67f97adb108262a134225f1
Nico Baier <nico.baier@gmail.com>
2023-12-04 20:56:05 -0600

    Refactor copyLink function and add new cards array

--- posts/test.js
+++ posts/test.js
@@ -1,14 +1,32 @@
-function copyLink(event) {
-  const href = window.location.href;
-  const headingId = event.target.querySelector("a").getAttribute("href");
-  const link = href + headingId;
-  alert(link);
-
-  // Create a temporary input element to copy the link
-  var tempInput = document.createElement("input");
-  tempInput.setAttribute("value", link);
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand("copy"); // Copy the link to clipboard
-  document.body.removeChild(tempInput); // Remove the temporary input element
-}
+const cards = [
+  {
+    card: 1,
+    numbers: [[""], [""]],
+    score: 4,
+  },
+  {
+    card: 2,
+    numbers: [[""], [""]],
+    score: 2,
+  },
+  {
+    card: 3,
+    numbers: [[""], [""]],
+    score: 2,
+  },
+  {
+    card: 4,
+    numbers: [[""], [""]],
+    score: 1,
+  },
+  {
+    card: 5,
+    numbers: [[""], [""]],
+    score: 0,
+  },
+  {
+    card: 6,
+    numbers: [[""], [""]],
+    score: 0,
+  },
+];
